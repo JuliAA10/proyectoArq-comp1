69c6b380836756f27f626334f2b9b10fefdb0226
Juli <juli.alvarezaguirre72@gmail.com>
2024-06-24 12:27:56 -0300

    login

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <pthread.h>
 #include <stdio.h>
 #include "EasyPIO.h"
+#include <ncurses.h>
+#include <string.h>
 
+#define PASSWORD "12345"
 #define QUIT_KEY 'q'
 #define DELAY_INTERVAL 250
 #define DEFAULT_DELAY 1000
@@ -21,6 +24,46 @@ void Delay(size_t a){
     a = a * 100000;
     while (a--)
         ;
+}
+
+unsigned int Login(){
+    char *right_password = PASSWORD;
+  char input[5];
+  int i = 0;
+  char c;
+  printf("Ingrese su password de 5 digitos: ");
+
+ while (1) {
+    c = getchar();
+
+    if (c == '\n') {
+      input[i] = '\0';
+      break;
+    } else if (c == 127 || c == '\b') {
+      if (i > 0) {
+        i--;
+        printf("\b \b");
+      }
+    } else {
+      if (i < 5) {
+        printf("*");
+        input[i] = c;
+        i++;
+      }
+    }
+    if (i == 5)
+      break;
+  }
+
+input[i] = '\0';
+
+if (strcmp(input, right_password) == 0) {
+  printf("\n\rAcceso concedido\n\r");
+  return 1;
+} else {
+  printf("\n\rAcceso denegado\n\r");
+  return 0;
+}
 }
 
 int main(){
